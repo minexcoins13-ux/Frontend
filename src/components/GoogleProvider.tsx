08ae09c6f14ac3dc0ca99e7a42f153f198c28950
786shahfaiz-lang <786shahfaiz@gmail.com>
2026-02-26 16:06:38 +0530

    Hardcode Google Client ID

--- src/components/GoogleProvider.tsx
+++ src/components/GoogleProvider.tsx
@@ -3,7 +3,8 @@
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function GoogleProvider({ children }: { children: React.ReactNode }) {
-    const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "";
+    // Hardcoding the client ID directly to avoid Next.js .env loading issues on the client
+    const clientId = "578386307045-ti35g9n4mnhcr20ak4hh257567efqduq.apps.googleusercontent.com";
 
     return (
         <GoogleOAuthProvider clientId={clientId}>
